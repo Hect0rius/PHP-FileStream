b2aef08b8328fc5dca14dc18c6a635caa4624173
Hect0r Xorius <sysop@pkx.pw>
2021-09-25 17:55:24 +0100

    Update Endian.php

--- Endian.php
+++ Endian.php
@@ -3,14 +3,15 @@
 /**************************************************************************************************\
 |* PHP FileStream Beta (0.1a) By Jed Harlen Hall (Hect0r Xorius)                                  *|
 |* Contact:                                                                                       *|
-|* - Twitter: @SirHect0rius                                                                      *|
-|* - Email: jed@hect0r.com                                                                    *|
+|* - Twitter: @SirHect0rius                                                                       *|
+|* - Email: jed@hect0r.com                                                                        *|
 |* License:                                                                                       *|
 |* - Free to use, you can make money out of this if you wish, you are not allowed to sell the io  *|
 |*   only, you have to write a project containing the io to sell, thats the rule, all I require   *|
 |*   is a shout out to me as Jed or Hect0r and this header remains in the io files without        *|
 |*   removal.                                                                                     *|
 |* - If I find any implementations without the license and no shout I will not be very happy and  *|
+|*   I'll send you a very nasty letter, mmmmkay :)                                                *|
 |* Shouts:                                                                                        *|
 |* Ayrus                                                                                          *|
 \**************************************************************************************************/
